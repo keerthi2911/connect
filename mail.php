d5c303671be9c5c721f9abd89d64a4d3d09ab33a
Keerthi G <90516801+keerthi2911@users.noreply.github.com>
2022-10-18 20:36:26 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "example@gmail.com";
+$recipient = "keerthi.g12345@gmail.com";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
