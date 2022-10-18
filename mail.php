38ea0b5e45ae81b403f4b429488fd558bd24eaa9
Keerthi G <90516801+keerthi2911@users.noreply.github.com>
2022-10-18 20:38:19 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -25,7 +25,7 @@ echo'
 <body>
     <div class="container">
         <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        <p class="back">Go back to the <a href="index.php">homepage</a>.</p>
         
     </div>
 </body>
