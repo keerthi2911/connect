6b8eab30ddd302568978bf2e971e109c3cf24ec7
Keerthi G <90516801+keerthi2911@users.noreply.github.com>
2022-10-18 18:48:44 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,26 +1,22 @@
 <?php
-
-	$firstName = $_POST['firstName'];
+//get data from form  
+$firstName = $_POST['firstName'];
 	$gender = $_POST['gender'];
 	$email = $_POST['email'];
 	$number = $_POST['number'];
 	$message = $_POST['message'];
-
-	// Database connection
-	$conn = new mysqli('sql6.freesqldatabase.com','sql6525760','yx7EVHzi6w','sql6525760');
-if($conn->connect_error){
-		// echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, gender, email,number, message) values(?, ?, ?, ?,?)");
-		$stmt->bind_param("sssis", $firstName, $gender, $email,$number, $message);
-	    $stmt->execute();
-		// echo $execval;
-		echo '<script>alert("Thank You..! Your Feedback is Valuable to Us"); location.replace(document.referrer);</script>';
-		$stmt->close();
-		$conn->close();
-	}
+$to = "keerthig12345@mail.com";
+$subject = "Mail From website";
+$txt ="FirstName = ". $firstName . "\r\n  Gender = " . $gender . "\r\n Email =" . $email . "\r\n Number =" . $number . "\r\n Message =" . $message;
+$headers = "From: noreply@vcetinfo.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+echo '<script>alert("Thank You..! Your Feedback is Valuable to Us"); location.replace(document.referrer);</script>';
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
