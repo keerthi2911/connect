8065bdf8f86da00717d81e03e1b360559f93f16c
Keerthi G <90516801+keerthi2911@users.noreply.github.com>
2022-10-19 18:48:44 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,18 +1,17 @@
 <?php
 //get data from form  
-
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
+$firstName = $_POST['firstName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$number = $_POST['number'];
+	$message = $_POST['message'];
 $to = "keerthi.g12345@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$subject = "Mail From codeconia";
+$txt ="FirstName = ". $firstName .  "\r\n Gender =" . $gender . "\r\n Email =" . $email . "\r\n Number =" . $number . "\r\n Message =" . $message;
 $headers = "From: noreply@codeconia.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
-//redirect
 echo '<script>alert("Thank You..! Your Feedback is Valuable to Us"); location.replace(document.referrer);</script>';
 ?>
-
